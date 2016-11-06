bd488d8924ef6292ced763fdbf94cfb071444a33
Caleb Kauffman <ckauffman16@gmail.com>
2016-11-06 15:39:57 -0800

    Added data population scripts

---
+++ Doc/Design/Database/DataPopulationUserLocations.sql
@@ -0,0 +1,28 @@
+﻿
+insert into UserLocations
+  SET UserID = 1, LocationsID = 2, InRange = FALSE, Active = FALSE;
+
+insert into UserLocations
+  SET UserID = 1, LocationsID = 3, InRange = FALSE, Active = FALSE;
+
+insert into UserLocations
+  SET UserID = 2, LocationsID = 1, InRange = FALSE, Active = FALSE;
+
+insert into UserLocations
+  SET UserID = 2, LocationsID = 4, InRange = FALSE, Active = FALSE;
+
+insert into UserLocations
+  SET UserID = 3, LocationsID = 10, InRange = FALSE, Active = FALSE;
+
+insert into UserLocations
+  SET UserID = 4, LocationsID = 70, InRange = FALSE, Active = FALSE;
+
+insert into UserLocations
+  SET UserID = 5, LocationsID = 15, InRange = FALSE, Active = FALSE;
+
+insert into UserLocations
+  SET UserID = 6, LocationsID = 12, InRange = FALSE, Active = FALSE;
+
+
+
+SELECT * FROM UserLocations;
---
+++ Doc/Design/Database/DataPopulationUser_Group.sql
@@ -0,0 +1,46 @@
+﻿SELECT * FROM Groups
+
+
+insert into User_Group
+  SET UserID = (SELECT UserID
+                FROM Users
+                WHERE UserName = 'rtorresc'),
+  GroupID = (SELECT GroupID
+            FROM Groups
+            WHERE GroupName = 'Welch and Sons');
+
+
+  insert into User_Group
+  SET UserID = 1, GroupID = 1;
+
+  insert into User_Group
+  SET UserID = 2, GroupID = 1;
+
+  insert into User_Group
+  SET UserID = 3, GroupID = 1;
+
+insert into User_Group
+  SET UserID = 4, GroupID = 1;
+
+insert into User_Group
+  SET UserID = 5, GroupID = 1;
+
+insert into User_Group
+  SET UserID = 6, GroupID = 1;
+
+insert into User_Group
+  SET UserID = 7, GroupID = 2;
+
+insert into User_Group
+  SET UserID = 8, GroupID = 2;
+
+insert into User_Group
+  SET UserID = 9, GroupID = 3;
+
+insert into User_Group
+  SET UserID = 10, GroupID = 3;
+
+insert into User_Group
+  SET UserID = 1, GroupID = 3;
+
+SELECT * FROM User_Group
