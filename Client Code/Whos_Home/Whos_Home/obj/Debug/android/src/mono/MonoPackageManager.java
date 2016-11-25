83a820b653512e4d14829d10273d8bcc6796f6a2
Cris Laney <crislaney@gmail.com>
2016-11-25 11:43:05 -0800

    SignUp

--- Client Code/Whos_Home/Whos_Home/obj/Debug/android/src/mono/MonoPackageManager.java
+++ Client Code/Whos_Home/Whos_Home/obj/Debug/android/src/mono/MonoPackageManager.java
@@ -99,6 +99,7 @@ class MonoPackageManager_Resources {
 	public static final String[] Assemblies = new String[]{
 		/* We need to ensure that "Whos_Home.dll" comes first in this list. */
 		"Whos_Home.dll",
+		"Java.Interop.dll",
 		"System.Threading.dll",
 		"System.Runtime.dll",
 		"System.Collections.dll",
@@ -109,8 +110,9 @@ class MonoPackageManager_Resources {
 		"System.Runtime.InteropServices.dll",
 		"System.Runtime.Extensions.dll",
 		"System.Reflection.Extensions.dll",
+		"System.ServiceModel.Internals.dll",
 	};
 	public static final String[] Dependencies = new String[]{
 	};
-	public static final String ApiPackageName = "Mono.Android.Platform.ApiLevel_24";
+	public static final String ApiPackageName = "Mono.Android.Platform.ApiLevel_23";
 }
