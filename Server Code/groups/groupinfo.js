fda1c91574da123a23cf5790dd0e50648773d1b6
michael.blake@oit.edu <michael.blake@oit.edu>
2017-01-25 22:58:23 -0800

    fixed errors with groupinfo and auth.checkInGroup

--- Server Code/groups/groupinfo.js
+++ Server Code/groups/groupinfo.js
@@ -5,17 +5,16 @@ var auth = require('./../middlewares/auth');
 module.exports = function (app) {
     //Authenticated route to get group information from group id
     //(\\d+) makes sure :id is an integer.
-    app.get('/groups/:id(\\d+)', auth.CheckAuthToken, function (req, res) {
+    app.get('/groups/:groupid(\\d+)', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
         var con = mysql.createConnection(config.connectionInfo);
         if(req.body.decoded)
         {
-            var request = "SELECT u.UserName, g.GroupName, (SELECT NetName FROM Group_Locations AS gl WHERE u.LocationID = gl.LocationID) AS LocationName \
+            var request = "SELECT u.UserName, g.GroupName, (SELECT NetName FROM Group_Locations WHERE u.LocationID = gl.LocationID) AS LocationName \
                             FROM Users u \
                             INNER JOIN User_Groups ug ON u.UserID = ug.UserID \
                             INNER JOIN Groups g ON ug.GroupID = g.GroupID \
-                            INNER JOIN Group_Locations gl ON g.GroupID = gl.GroupID \
-                            WHERE g.GroupID = " + con.escape(req.params.id); + " \
-                            AND u.UserID = " + req.body.decoded.UserID;
+                            LEFT JOIN Group_Locations gl ON g.GroupID = gl.GroupID \
+                            WHERE g.GroupID = " + con.escape(req.params.groupid);
             con.query(request, function(err, result) {
                 res.json(result);
                 res.end();
