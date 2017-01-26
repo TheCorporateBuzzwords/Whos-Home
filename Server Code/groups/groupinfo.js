a5b71ee720da88b89b3b2ee5c351c50993b15800
michael.blake@oit.edu <michael.blake@oit.edu>
2017-01-25 17:13:43 -0800

    added location info to groupinfo endpoint

--- Server Code/groups/groupinfo.js
+++ Server Code/groups/groupinfo.js
@@ -9,10 +9,11 @@ module.exports = function (app) {
         var con = mysql.createConnection(config.connectionInfo);
         if(req.body.decoded)
         {
-            var request = "SELECT u.UserName, g.GroupName \
+            var request = "SELECT u.UserName, g.GroupName, (SELECT NetName FROM Group_Locations AS gl WHERE u.LocationID = gl.LocationID) AS LocationName \
                             FROM Users u \
                             INNER JOIN User_Groups ug ON u.UserID = ug.UserID \
                             INNER JOIN Groups g ON ug.GroupID = g.GroupID \
+                            INNER JOIN Group_Locations gl ON g.GroupID = gl.GroupID \
                             WHERE g.GroupID = " + con.escape(req.params.id); + " \
                             AND u.UserID = " + req.body.decoded.UserID;
             con.query(request, function(err, result) {
