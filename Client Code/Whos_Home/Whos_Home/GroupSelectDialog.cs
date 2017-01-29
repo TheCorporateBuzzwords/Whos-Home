af42eab27368bc2e85a0bd1abf32d3c57d907772
Caleb Kauffman <ckauffman16@gmail.com>
2017-01-28 18:33:34 -0800

    Update GroupSelectDialog.cs

--- Client Code/Whos_Home/Whos_Home/GroupSelectDialog.cs
+++ Client Code/Whos_Home/Whos_Home/GroupSelectDialog.cs
@@ -9,6 +9,7 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Newtonsoft.Json;
 
 namespace Whos_Home
 {
@@ -17,20 +18,33 @@ namespace Whos_Home
         private Button Select;
         private Button Cancel;
         private Button AddUser;
+        private TextView textviewGroupName;
+        private string groupname;
+        public GroupSelectDialog(string group_name)
+        {
+            this.groupname = group_name;
+            
+        }
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             base.OnCreateView(inflater, container, savedInstanceState);
 
-            var view = inflater.Inflate(Resource.Layout.GroupDialog, container, false);
+            View view = inflater.Inflate(Resource.Layout.GroupDialog, container, false);
 
+            //find buttons in layout 
             Select = view.FindViewById<Button>(Resource.Id.buttonSelectGroup);
             Cancel = view.FindViewById<Button>(Resource.Id.buttonCancelSelectGroup);
             AddUser = view.FindViewById<Button>(Resource.Id.buttonAddUserToGroup);
 
+            //find textview that contains group name and set it to value passed in constructor
+            textviewGroupName = view.FindViewById<TextView>(Resource.Id.textviewGroupDialog);
+            textviewGroupName.Text = groupname;
+
             //Add click functions for buttons
-            Select.Click += Select_Click;
             Cancel.Click += Cancel_Click;
             AddUser.Click += AddUser_Click;
+            Select.Click += Select_Click;
 
             return view;
 
@@ -38,8 +52,23 @@ namespace Whos_Home
 
         private void Select_Click(object sender, EventArgs e)
         {
-            DB_Singleton db = DB_Singleton.Instance;
-            //db.ChangeActiveGroup()
+            AlertDialog.Builder alert = new AlertDialog.Builder(this.Context);
+            alert.SetTitle("Make " + groupname + " your current group?");
+
+            //Set default group
+            alert.SetPositiveButton("Confirm", (senderAlert, args) => 
+            {
+                //This is where current group is set
+            });
+
+            //Close dialog
+            alert.SetNegativeButton("Cancel", (senderAlert, args) =>
+            {
+                Dismiss();
+            });
+            Dialog dialog = alert.Create();
+            dialog.Show();
+            Dismiss();
         }
 
         private void AddUser_Click(object sender, EventArgs e)
