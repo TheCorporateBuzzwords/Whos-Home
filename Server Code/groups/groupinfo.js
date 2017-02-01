8889b722d18ad73c54550e1426bea9b50ee499a7
michael.blake@oit.edu <michael.blake@oit.edu>
2017-01-31 19:55:27 -0800

    removed a space causing an error

--- Server Code/groups/groupinfo.js
+++ Server Code/groups/groupinfo.js
@@ -8,11 +8,10 @@ module.exports = function (app) {
     app.get('/groups/:groupid(\\d+)', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
         var con = mysql.createConnection(config.connectionInfo);
         if (req.body.decoded) { //Had to do a left join in this query or else nothing would be returned if a group had no locations.
-            var request = "SELECT u.UserName, g.GroupName, (SELECT NetName FROM Group_Locations WHERE u.LocationID = gl.LocationID) AS LocationName \
-                            FROM Users u \
+            var request = "SELECT u.UserName, g.GroupName, (SELECT NetName FROM Group_Locations WHERE u.LocationID = gl.LocationID) AS LocationName FROM Users u \
                             INNER JOIN User_Groups ug ON u.UserID = ug.UserID \
                             INNER JOIN Groups g ON ug.GroupID = g.GroupID \
-                            LEFT JOIN Group_Locations gl ON g.GroupID = gl.GroupID \ 
+                            LEFT JOIN Group_Locations gl ON g.GroupID = gl.GroupID \
                             WHERE g.GroupID = " + con.escape(req.params.groupid);
             con.query(request, function (err, result) {
                 return res.json(result);
