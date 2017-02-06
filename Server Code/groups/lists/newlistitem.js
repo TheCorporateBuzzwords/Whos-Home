330fc27ec2170339a05a6e9488eeccf4b9e3195b
michael.blake@oit.edu <michael.blake@oit.edu>
2017-02-05 17:52:19 -0800

    added newlistitem.js

---
+++ Server Code/groups/lists/newlistitem.js
@@ -0,0 +1,25 @@
+var config = require("./../../config");
+var mysql = require("mysql");
+var auth = require('./../../middlewares/auth');
+
+module.exports = function (app) {
+    app.post('/groups/:groupid(\\d+)/list/:listid(\\d+)', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
+        var con = mysql.createConnection(config.connectionInfo);
+        if(req.params.groupid && req.body.content)
+        {
+            var insertRequest = "INSERT INTO Items (GroupID, UserID, ListID, ItemText, PostTime) values (" + con.escape(req.params.groupid) + ", " + req.body.decoded.UserID + ", " + ", " + req.params.listid +  ", " + con.escape(req.body.content) + ", " + "CURRENT_TIME()" + ");";
+            con.query(insertRequest, function(err, result) {
+                if(err) {
+                    console.log(err);
+                    return res.end();
+                }
+                else {
+                    return res.status(200).json({ status: "success", message: "succesfully added list item"});
+                }
+            });
+        }
+        else {
+            return res.status(400).json({ status: "error", message: "missing parameter in POST request" });
+        }
+    });
+}
