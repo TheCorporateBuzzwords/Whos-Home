3fdc9530e383aa964782944c1e757a1514947e82
michael.blake@oit.edu <michael.blake@oit.edu>
2017-02-21 19:04:58 -0800

    added extra information to get lists endpoint

--- Server Code/groups/lists/getlists.js
+++ Server Code/groups/lists/getlists.js
@@ -5,7 +5,7 @@ var auth = require('./../../middlewares/auth');
 module.exports = function (app) {
     app.get('/groups/:groupid(\\d+)/lists/', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
         var con = mysql.createConnection(config.connectionInfo);
-            var insertRequest = "SELECT * FROM Lists WHERE GroupID = " + req.params.groupid;
+            var insertRequest = "SELECT Lists.ListID, Lists.GroupID, Lists.UserID, Lists.Title, Lists.PostTime, Users.UserName, Users.FirstName, Users.LastName FROM Lists INNER JOIN Users ON Lists.UserID = Users.UserID WHERE GroupID = " + req.params.groupid;
             con.query(insertRequest, function(err, result) {
                 if(err) {
                     console.log(err);
