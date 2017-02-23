997bfdb997696c52de245b486c6e01084035a9b9
michael.blake@oit.edu <michael.blake@oit.edu>
2017-02-22 16:43:18 -0800

    fixed abiguiouty error

--- Server Code/groups/lists/getlistitems.js
+++ Server Code/groups/lists/getlistitems.js
@@ -5,7 +5,7 @@ var auth = require('./../../middlewares/auth');
 module.exports = function (app) {
     app.get('/groups/:groupid(\\d+)/lists/:listid(\\d+)', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
         var con = mysql.createConnection(config.connectionInfo);
-            var selectQuery = "SELECT ItemID, ListID, UserID, ItemText, Completed, PostTime, UserName, FirstName, LastName FROM Items INNER JOIN Users ON Items.UserID = Users.UserID WHERE ListID = " + req.params.listid;
+            var selectQuery = "SELECT Items.ItemID, Items.ListID, Items.UserID, Items.ItemText, Items.Completed, Items.PostTime, Users.UserName, Users.FirstName, Users.LastName FROM Items INNER JOIN Users ON Items.UserID = Users.UserID WHERE ListID = " + req.params.listid;
             con.query(selectQuery, function(err, result) {
                 if(err) {
                     console.log(err);
