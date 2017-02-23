22d3fa4951adca90f7aa225fdec95dbe5073decf
michael.blake@oit.edu <michael.blake@oit.edu>
2017-02-22 16:22:19 -0800

    fixed getlistitems bug

--- Server Code/groups/lists/getlistitems.js
+++ Server Code/groups/lists/getlistitems.js
@@ -5,8 +5,8 @@ var auth = require('./../../middlewares/auth');
 module.exports = function (app) {
     app.get('/groups/:groupid(\\d+)/lists/:listid(\\d+)', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
         var con = mysql.createConnection(config.connectionInfo);
-            var insertRequest = "SELECT ItemID, ListID, UserID, ItemText, Completed, PostTime, UserName, FirstName, LastName FROM Items INNER JOIN Users ON Items.UserID = Users.UserID WHERE ListID = " + req.params.listid;
-            con.query(insertRequest, function(err, result) {
+            var selectQuery = "SELECT ItemID, ListID, UserID, ItemText, Completed, PostTime, UserName, FirstName, LastName FROM Items INNER JOIN Users ON Items.UserID = Users.UserID WHERE ListID = " + req.params.listid;
+            con.query(selectQuery, function(err, result) {
                 if(err) {
                     console.log(err);
                     return res.end();
