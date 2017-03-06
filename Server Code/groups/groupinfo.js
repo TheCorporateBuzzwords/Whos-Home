b10c23455987b5977a8c735c55f4643b8d2d76a8
michael.blake@oit.edu <michael.blake@oit.edu>
2017-03-06 00:59:07 -0800

    attempt at fixing groupinfo bug

--- Server Code/groups/groupinfo.js
+++ Server Code/groups/groupinfo.js
@@ -8,11 +8,11 @@ module.exports = function (app) {
     app.get('/groups/:groupid(\\d+)', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
         //var con = mysql.createConnection(config.connectionInfo);
         if (req.body.decoded) { //Had to do a left join in this query or else nothing would be returned if a group had no locations.
-            var request = "SELECT DISTINCT u.UserName, (SELECT NetName FROM Group_Locations WHERE u.LocationID = gl.LocationID) AS LocationName FROM Users u \
+            var request = "SELECT DISTINCT u.UserName, g.GroupName, gl.NetName FROM Users u \
                             INNER JOIN User_Groups ug ON u.UserID = ug.UserID \
                             INNER JOIN Groups g ON ug.GroupID = g.GroupID \
                             LEFT JOIN Group_Locations gl ON g.GroupID = gl.GroupID \
-                            WHERE g.GroupID = " + config.pool.escape(req.params.groupid);
+                            WHERE g.GroupID = " + config.pool.escape(req.params.groupid) + " AND gl.LocationID = u.LocationID";
             config.pool.query(request, function (err, result) {
                 return res.json(result);
             });
