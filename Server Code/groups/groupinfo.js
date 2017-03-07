f8af5b8b5da350212be6e071bb90260c55c393b2
michael.blake@oit.edu <michael.blake@oit.edu>
2017-03-06 19:52:31 -0800

    new select query for group info

--- Server Code/groups/groupinfo.js
+++ Server Code/groups/groupinfo.js
@@ -9,11 +9,11 @@ module.exports = function (app) {
         //var con = mysql.createConnection(config.connectionInfo);
         if (req.body.decoded) { //Had to do a left join in this query or else nothing would be returned if a group had no locations.
             var request = "SELECT UserName, GroupName, NetName\
-  FROM Users\
-  JOIN User_Groups ON Users.UserID = User_Groups.UserID\
-  JOIN Groups ON User_Groups.GroupID = Groups.GroupID\
-  JOIN Group_Locations ON Group_Locations.LocationID = Users.LocationID OR Users.LocationID = NULL\
-  WHERE Groups.GroupID =" + config.pool.escape(req.params.groupid);
+                            FROM Users\
+                            JOIN User_Groups ON Users.UserID = User_Groups.UserID\
+                            JOIN Groups ON User_Groups.GroupID = Groups.GroupID\
+                            LEFT JOIN Group_Locations ON Group_Locations.GroupID = Groups.GroupID\
+                            WHERE Groups.GroupID = " + config.pool.escape(req.params.groupid);
             config.pool.query(request, function (err, result) {
                 return res.json(result);
             });
