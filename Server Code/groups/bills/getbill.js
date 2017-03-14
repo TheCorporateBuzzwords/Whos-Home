22ca4584ee3a8ff6ccfb279c313437a3b7436418
michael.blake@oit.edu <michael.blake@oit.edu>
2017-03-14 13:33:54 -0700

    fixed bug with getbills

--- Server Code/groups/bills/getbill.js
+++ Server Code/groups/bills/getbill.js
@@ -3,13 +3,13 @@ var mysql = require("mysql");
 var auth = require('./../../middlewares/auth');
 
 module.exports = function (app) {
-    app.get('/groups/:groupid(\\d+)/bills/:billid(\\d+)', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
+    app.get('/groups/:groupid(\\d+)/bills/', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
         if (req.query.recipient)
             var selectQuery = "SELECT BillID, GroupID, SenderID, RecipientID, CategoryID, Title, Description, Amount, DATE_FORMAT(DateDue, '%c/%d/%Y %r:%h:%s') AS DateDue \
-                               FROM Bills WHERE GroupId = " + req.params.groupid + " AND BillId = " + req.params.billid + " AND RecipientId = " + req.query.recipient;
+                               FROM Bills WHERE GroupId = " + req.params.groupid + " AND RecipientId = " + req.query.recipient;
         else
             var selectQuery = "SELECT BillID, GroupID, SenderID, RecipientID, CategoryID, Title, Description, Amount, DATE_FORMAT(DateDue, '%c/%d/%Y %r:%h:%s') AS DateDue \
-                               FROM Bills WHERE GroupId = " + req.params.groupid + " AND BillId = " + req.params.billid;
+                               FROM Bills WHERE GroupId = " + req.params.groupid;
         console.log(selectQuery);
         config.pool.query(selectQuery, function(err, result) {
             if(err) {
