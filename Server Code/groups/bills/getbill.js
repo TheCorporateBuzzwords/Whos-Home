6730996c2bd28de0ab90fbe88f8070313faf286c
michael.blake@oit.edu <michael.blake@oit.edu>
2017-03-14 14:29:02 -0700

    added usernames to getbills endpoint

--- Server Code/groups/bills/getbill.js
+++ Server Code/groups/bills/getbill.js
@@ -5,11 +5,17 @@ var auth = require('./../../middlewares/auth');
 module.exports = function (app) {
     app.get('/groups/:groupid(\\d+)/bills/', [auth.CheckAuthToken, auth.CheckInGroup], function (req, res) {
         if (req.query.recipient)
-            var selectQuery = "SELECT BillID, GroupID, SenderID, RecipientID, CategoryID, Title, Description, Amount, DATE_FORMAT(DateDue, '%c/%d/%Y %r:%h:%s') AS DateDue \
-                               FROM Bills WHERE GroupId = " + req.params.groupid + " AND RecipientId = " + req.query.recipient;
+            var selectQuery = "SELECT BillID, GroupID, u1.UserName AS Sender, u2.UserName AS Recipient, CategoryID, Title, Description, Amount, DATE_FORMAT(DateDue, '%c/%d/%Y %r:%h:%s') AS DateDue \
+                               FROM Bills b \
+                               INNER JOIN Users u1 ON b.RecipientID = u1.UserID \
+                               INNER JOIN Users u2 ON b.SenderID = u2.UserID \
+                               WHERE GroupId = " + req.params.groupid + " AND RecipientId = " + req.query.recipient;
         else
-            var selectQuery = "SELECT BillID, GroupID, SenderID, RecipientID, CategoryID, Title, Description, Amount, DATE_FORMAT(DateDue, '%c/%d/%Y %r:%h:%s') AS DateDue \
-                               FROM Bills WHERE GroupId = " + req.params.groupid;
+            var selectQuery = "SELECT BillID, GroupID, u1.UserName AS Sender, u2.UserName AS Recipient, CategoryID, Title, Description, Amount, DATE_FORMAT(DateDue, '%c/%d/%Y %r:%h:%s') AS DateDue \
+                               FROM Bills b \
+                               INNER JOIN Users u1 ON b.RecipientID = u1.UserID \
+                               INNER JOIN Users u2 ON b.SenderID = u2.UserID \
+                               WHERE GroupId = " + req.params.groupid;
         console.log(selectQuery);
         config.pool.query(selectQuery, function(err, result) {
             if(err) {
