ac8e49a4c7b1811a57e5b2106dd3c19510448b0d
michael.blake@oit.edu <michael.blake@oit.edu>
2017-05-16 18:30:43 -0700

    fixed typo

--- Server Code/routes/users.js
+++ Server Code/routes/users.js
@@ -202,7 +202,7 @@ router.post('/', function (req, res) {
 
 router.put('/fcm', auth.CheckAuthToken, function (req, res) {
     if (req.body.regToken) {
-        var requst = "UPDATE Users SET FirebaseToken = " + config.pool.escape(req.body.regToken) +
+        var request = "UPDATE Users SET FirebaseToken = " + config.pool.escape(req.body.regToken) +
                      " WHERE UserID = " + req.body.decoded.UserID;
         config.pool.query(request, function(err, result) {
             if (err) {
